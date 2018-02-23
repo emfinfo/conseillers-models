8aca6abc31239cc66fe1ef40c7915da8c17b0284
jcstritt <strittjc@edufr.ch>
2018-02-23 17:23:20 +0100

    New Login constructor

--- src/main/java/models/Login.java
+++ src/main/java/models/Login.java
@@ -1,10 +1,13 @@
 package models;
 
+import ch.jcsinfo.system.InObject;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import java.io.Serializable;
+import java.time.Instant;
+import java.util.Date;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -12,12 +15,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 /**
  * Classe entité.
- * 
+ *
  * @author jcstritt
  */
 @Entity
@@ -39,12 +44,20 @@ public class Login implements Serializable {
   @Column(name = "nom")
   private String nom;
 
+  @Basic(optional = false)
+  @Column(name = "domaine")
+  private String domaine;
+
+  @Basic(optional = false)
   @Column(name = "motDePasse")
   @JsonProperty(access = Access.WRITE_ONLY) // nouveau JCS 13.6.2017 au lieu de JsonIgnore
   private String motDePasse;
 
-  @Column(name = "domaine")
-  private String domaine;
+  @Basic(optional = false)
+  @Column(name = "loginTimestamp")
+  @Temporal(TemporalType.TIMESTAMP)
+  @JsonIgnore
+  private Date timestamp;
 
   @Column(name = "profil")
   private String profil;
@@ -58,9 +71,29 @@ public class Login implements Serializable {
   @Column(name = "langue")
   private String langue;
 
+  public Login(String nom, String domaine, String motDePasse, String profil, String email, String initiales, String lang) {
+    this.pkLogin = 0;
+    this.nom = nom;
+    this.domaine = domaine;
+    this.motDePasse = motDePasse;
+    this.profil = profil;
+    this.email = email;
+    this.initiales = initiales;
+    this.langue = langue;
+    this.timestamp = new Date(Instant.now().toEpochMilli());
+  }
+
+  public Login() {
+    this("", "", "", null, null, null, null);
+  }
+
   @Override
   public String toString() {
     return nom + " (" + langue + ")";
+  }
+
+  public String toString2() {
+    return InObject.fieldsToString(this);
   }
 
 }
